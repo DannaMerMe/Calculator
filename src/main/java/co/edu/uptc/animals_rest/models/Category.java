992561e3bc587787ba8fcf5b9c537a6607eae575
Juan Rodriguez <juansebastianrodriguezmateus7b@gmail.com>
2024-09-26 19:06:58 -0500

    Add factures

---
+++ src/main/java/co/edu/uptc/animals_rest/models/Category.java
@@ -0,0 +1,16 @@
+package co.edu.uptc.animals_rest.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Category {
+
+    private String category;
+    private int number;
+
+}
